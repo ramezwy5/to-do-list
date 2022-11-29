8531967d34ca9e7815594231d35d0b7c9d0b52ba
Kaaper <ramez4fx@gmail.com>
2022-11-29 16:48:33 -0500

    to-do-list

---
+++ script.js
@@ -0,0 +1,73 @@
+
+var addedList = $("#list"); //ul list element
+var itemAdded = $("#input"); // input field
+const submitBtn = $(".submit"); // submit button (+).
+const reset = $('#reset'); // reset button.
+//const showList = $(".list"); // the div contains ul list element for style
+
+//need a list of tasks has id for each task.
+let tasksList = [];
+
+//  update tasksList with localStorage
+if(localStorage.getItem("tasksArr")){
+    tasksList = JSON.parse(localStorage.getItem("tasksArr"))
+}
+getLS();
+
+
+// submit event listeners.
+
+//submit button
+submitBtn.on("click", function(){
+        addToList();
+    });
+// Enter key
+$("body").keydown(function(event){
+    if(event.key === "Enter"){
+        addToList();
+    }
+});
+
+
+
+// rest button
+reset.on("click", () => {window.localStorage.clear(); tasksList = []; $(".task").remove()});
+
+// focus on input
+window.onload = () => itemAdded.focus();
+
+// create object for tasks has id and text.
+function addToList(){
+    if(itemAdded.val() !== ""){
+        let taskObj = {id: Date.now(), text: itemAdded.val()};
+        tasksList.push(taskObj); // add object to tasksList        
+        taskItem(taskObj.text);
+        itemAdded.val("");
+        saveToLS(tasksList);
+    }
+}    
+// creating element for task in task list
+
+function taskItem(taskAdd){
+   addedList.append(`<li class="task">${taskAdd}</li>`);
+   $(".task").css("cursor", "pointer");
+    
+    // complete task
+    $("li.task").on("click",function (){
+        $(this).addClass("complete");
+    })
+}
+
+function saveToLS(tasks){
+    window.localStorage.setItem("tasksArr", JSON.stringify(tasks));
+}
+
+function getLS(){
+    let data = window.localStorage.getItem("tasksArr");
+    if(data){
+        let localTasks = JSON.parse(data);
+        localTasks.forEach(t => {
+            taskItem(t.text);
+        });
+    }
+}
